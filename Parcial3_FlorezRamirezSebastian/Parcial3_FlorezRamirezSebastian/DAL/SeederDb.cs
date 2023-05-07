3480bf98fab93674e0a8eee095da8f5ab57fe9a9
SEBASTIAN FLOREZ RAMIREZ <sebastianflorez303254@correo.itm.edu.co>
2023-05-07 13:02:41 -0500

    Create Seeder Service

---
+++ Parcial3_FlorezRamirezSebastian/Parcial3_FlorezRamirezSebastian/DAL/SeederDb.cs
@@ -0,0 +1,35 @@
+﻿using Parcial3_FlorezRamirezSebastian.DAL.Entities;
+
+namespace Parcial3_FlorezRamirezSebastian.DAL
+{
+    public class SeederDb
+    {
+        private readonly DatabaseContext _context;
+
+        public SeederDb(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync(); //reemplaza el comando update-database
+            await PopulateServicesAsync();
+        }
+
+        private async Task PopulateServicesAsync()
+        {
+           if(!_context.Services.Any()) //! any si no hay ninguno
+            {
+                _context.Services.Add(new Service { Name = "Lavada simple", Price = 25000 });
+                _context.Services.Add(new Service { Name = "Lavada + Polishada", Price = 50000 });
+                _context.Services.Add(new Service { Name = "Lavada + Aspirada de Cojinería", Price = 30000 });
+                _context.Services.Add(new Service { Name = "Lavada Full", Price = 65000 });
+                _context.Services.Add(new Service { Name = "Lavada en seco del Motor", Price = 80000 });
+                _context.Services.Add(new Service { Name = "Lavada Chasis", Price = 90000 });
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}

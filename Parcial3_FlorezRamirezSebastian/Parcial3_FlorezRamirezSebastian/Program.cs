af41fc32e192d64086df5c08599bb7fc5d6bf6ce
SEBASTIAN FLOREZ RAMIREZ <sebastianflorez303254@correo.itm.edu.co>
2023-05-07 15:46:09 -0500

    configurate

--- Parcial3_FlorezRamirezSebastian/Parcial3_FlorezRamirezSebastian/Program.cs
+++ Parcial3_FlorezRamirezSebastian/Parcial3_FlorezRamirezSebastian/Program.cs
@@ -1,5 +1,9 @@
 using Parcial3_FlorezRamirezSebastian.DAL;
 using Microsoft.EntityFrameworkCore;
+using Parcial3_FlorezRamirezSebastian.Helpers;
+using Parcial3_FlorezRamirezSebastian.Service;
+using Microsoft.AspNetCore.Identity;
+using Parcial3_FlorezRamirezSebastian.DAL.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +15,34 @@ builder.Services.AddDbContext<DatabaseContext>(o =>
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddIdentity<User, IdentityRole>(io =>//aca se configura las restricciones password
+{
+    io.User.RequireUniqueEmail = true; //Valida que el email no esté repetido
+    io.Password.RequireDigit = false;
+    io.Password.RequiredUniqueChars = 0;
+    io.Password.RequireLowercase = false;
+    io.Password.RequireNonAlphanumeric = false;
+    io.Password.RequireUppercase = false;
+    io.Password.RequiredLength = 6;
+}).AddEntityFrameworkStores<DatabaseContext>();
+
+builder.Services.AddTransient<SeederDb>();
+builder.Services.AddScoped<IUserHelper, UserHelper>();
+
+
 var app = builder.Build();
+
+SeederData();
+void SeederData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    {
+        SeederDb? service = scope.ServiceProvider.GetService<SeederDb>();
+        service.SeederAsync().Wait();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -25,7 +56,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
